f3792d773829e19a45a8d958cc9317104f7cf619
Vivek Mandava <vivekmandava@Emma-2.local>
2022-10-30 18:35:11 -0400

    JS test for introduction

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-document.onscroll
+
+//Change introduction h2 background color on mouse down and up
+const first = document.getElementById('intro');
+let changeColor = (event) =>{
+    event.target.style.backgroundColor = '#FF6A3D';
+    
+};
+let changeBack = (event) =>{
+    event.target.style.backgroundColor = '';
+}
+first.addEventListener('mousedown', changeColor);
+first.addEventListener('mouseup', changeBack)
+
+
+//Change NanceCademy background color on mouse down and up
+
+const eventHand = document.getElementById('Nance');
+
+eventHand.addEventListener('mouseover', changeColor);
+eventHand.addEventListener('mouseout', changeBack);
